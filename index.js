920695f3e79de34ef776024ce46206f3c63f4390
Wayne Huang <zjason4202@gmail.com>
2022-03-25 14:45:05 +0800

    1.0.0-alpha

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const Koa = require('koa');
 const path = require('path');
 const crypto = require('crypto');
+const mongoose = require('mongoose');
 const line = require('@line/bot-sdk');
 const bodyParser = require('koa-bodyparser');
+const { slice, each, map, chain, isNaN, toNumber, sample } = require('lodash');
 
+const WEEKDAYS = ['日', '一', '二', '三', '四', '五', '六'];
+
+// Dotenv
 require('dotenv').config({ path: path.join(__dirname, '.env.local') });
 
+// Koa
 const app = new Koa();
 app.use(bodyParser());
 
+// Mongoose
+mongoose.connect(process.env.MONGODB_URI);
+
+const shopSchema = new mongoose.Schema({
+  name: String,
+  sourceId: String,
+  closed: [Number],
+  rate: Number
+});
+
+const Shop = mongoose.model('shop', shopSchema);
+
+// Line
 const client = new line.Client({
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 });
@@ -17,6 +36,14 @@ function replyMessage(replyToken, text) {
   client.replyMessage(replyToken, { type: 'text', text })
 }
 
+function parseShop({ name, closed, rate }) {
+  const remark = [];
+  if (closed.length) remark.push(`休：${map(closed, day => WEEKDAYS[day]).join('、')}`);
+  remark.push(`機率：${rate}`);
+  return `${name}（${remark.join('，')}）`;
+}
+
+// Koa
 app.use(async ({ method, status }, next) => {
   try {
     if (method !== 'POST') throw '';
@@ -36,24 +63,58 @@ app.use(async ({ request }, next) => {
 });
 
 app.use(async ({ request }) => {
-  request.body?.events?.forEach(({ type, message, replyToken }) => {
-    // source.type user/group source.userId/source.groupId
+  request.body?.events?.forEach(async ({ type, message, source, replyToken }) => {
+    //t leaved group or nobody in group // source.type === 'group'
     if (type === 'message') {
-      const { text } = message;
-      const cmd = text.split(' ');
+      const sourceId = source.userId || source.groupId;
+      const cmd = message.text.split(' ');
+      const name = cmd[1];
       switch (cmd[0]) {
         case '戳':
-          replyMessage(replyToken, '戳屁戳');
+          console.log(sourceId);
+          replyMessage(replyToken, `戳屁戳（版本：v${require('./package.json').version}）`);
           break;
         case '有啥':
-          replyMessage(replyToken, '沒有');
+          replyMessage(replyToken,
+            map(await Shop.find({ sourceId }), parseShop).join('\n') || '不知道（選項尚未建立）'); // timezone
           break;
         case '可吃':
-          // cmd[1], cmd[2]
-          replyMessage(replyToken, '好');
+          if (await Shop.findOne({ name, sourceId })) {
+            replyMessage(replyToken, `不要（${name} 已存在）`);
+          } else {
+            let closed = [];
+            let rate = 1;
+            each(slice(cmd, 2), c => {
+              switch (c[0]) {
+                case '-':
+                  closed = chain([...c].sort()).sortedUniq().map(day => day % 7).filter(day => !isNaN(day)).value();
+                  break;
+                case '.':
+                  rate = toNumber(c) || 1;
+                  break;
+              }
+            })
+            const shop = new Shop({ name, sourceId, closed, rate });
+            replyMessage(replyToken, `好（${parseShop(await shop.save())} 已建立）`);
+          }
           break;
         case '吃啥':
-          replyMessage(replyToken, '不知道');
+          //t? help deciding
+          const filterList = chain(cmd).filter(c => c[0] === '-').map(c => slice(c, 1).join('')).value();
+          const shops = await Shop.find({ name: { $nin: filterList }, sourceId, closed: { $ne: new Date().getDay() } })
+          let shop;
+          do {
+            shop = sample(shops);
+          } while (shop && shop.rate < Math.random());
+          replyMessage(replyToken, shop ? parseShop(shop) : '不知道（沒有選項）');
+          break;
+        case '不吃':
+          replyMessage(replyToken,
+            (await Shop.deleteOne({ name, sourceId })).deletedCount ? `不吃就不吃（${name} 已刪除）` : `你要確定欸（${name} 不存在）`);
+          break;
+        case '不吃了':
+          await Shop.deleteMany({ sourceId });
+          replyMessage(replyToken, '不吃就不吃（已清除）');
           break;
       }
     }
