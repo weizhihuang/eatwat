b2d3f513d778c3d2da6b766c40795cf5e3218351
Wayne Huang <zjason4202@gmail.com>
2022-03-30 15:47:19 +0800

    1.1.0-alpha

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const crypto = require('crypto');
 const mongoose = require('mongoose');
 const line = require('@line/bot-sdk');
 const bodyParser = require('koa-bodyparser');
-const { each, map, chain, toNumber, sample } = require('lodash');
+const { each, map, chain, toNumber, sample, pick } = require('lodash');
 
 const WEEKDAYS = ['日', '一', '二', '三', '四', '五', '六'];
 const MAX_SHOP_NAME_LEN = 30;
@@ -34,7 +34,8 @@ const client = new line.Client({
 });
 
 function replyMessage(replyToken, text) {
-  client.replyMessage(replyToken, { type: 'text', text })
+  if (text)
+    client.replyMessage(replyToken, { type: 'text', text });
 }
 
 function parseShop({ name, closed, rate }) {
@@ -59,73 +60,72 @@ app.use(async ({ request }, next) => {
 });
 
 app.use(async ctx => {
-  each(ctx.request.body?.events, (async ({ type, message, source, replyToken }) => {
+  each(ctx.request.body?.events, async ({ type, message, source, replyToken }) => {
     //t blocked or deleted // source.type === 'user'
     //t leaved group or nobody in group? // source.type === 'group'
     if (type === 'message') {
       const sourceId = source.groupId || source.userId;
-      const params = chain(message.text).split(' ').filter(Boolean).value();
-      const cmd = params[0];
-      const name = params[1] || '';
+      replyMessage(replyToken, (await Promise.all(map(message.text.split('\n'), async text => {
+        const params = chain(text).split(' ').filter(Boolean).value();
+        const cmd = params[0];
+        const name = params[1] || '';
 
-      switch (cmd) {
-        case '戳':
-          console.log(sourceId);
-          replyMessage(replyToken, `戳屁戳（版本：v${require('./package.json').version}）`);
-          break;
-        case '有啥':
-          replyMessage(replyToken,
-            map(await Shop.find({ sourceId }), parseShop).join('\n') || '不知道（選項尚未建立）');
-          break;
-        case '可吃':
-          if (!name || await Shop.findOne({ name, sourceId })) {
-            replyMessage(replyToken, `不要（${name} 建立失敗）`);
-          } else if (name.length > MAX_SHOP_NAME_LEN) {
-            replyMessage(replyToken, `不要（${name.slice(0, 15)}... 建立失敗）`);
-          } else {
-            let closed = [];
-            let rate = 1;
-            chain(params).slice(2).each(param => {
-              switch (param[0]) {
-                case '-':
-                  closed = chain([...param].sort()).map(day => day % 7).sortedUniq().filter(day => !isNaN(day)).value();
-                  break;
-                case '.':
-                  rate = toNumber(param) || rate;
-                  break;
-              }
-            }).value();
-            const shop = new Shop({ name, sourceId, closed, rate });
-            replyMessage(replyToken, `好（${parseShop(await shop.save())} 已建立）`);
-          }
-          break;
-        case '吃啥':
-          //t? help deciding
-          const filterList = chain(params).filter(param => param[0] === '-').map(param => param.slice(1)).value();
-          const shops = await Shop.find({ name: { $nin: filterList }, sourceId, closed: { $ne: new Date().getDay() } }) // timezone
-          let shop;
-          do {
-            shop = sample(shops);
-          } while (shop && shop.rate < Math.random());
-          replyMessage(replyToken, shop ? parseShop(shop) : '不知道（沒有選項）');
-          break;
-        case '不吃':
-          replyMessage(replyToken,
-            (await Shop.deleteOne({ name, sourceId })).deletedCount ? `不吃就不吃（${name} 已刪除）` : `你要確定欸（${name} 不存在）`);
-          break;
-        case '不吃了':
-          await Shop.deleteMany({ sourceId });
-          replyMessage(replyToken, '不吃就不吃（已清除）');
-          break;
-        case '怎麼吃':
-          break;
-        default:
-          if (source.type === 'user')
-            replyMessage(replyToken, '公鯊小');
-          break;
-      }
+        switch (cmd) {
+          case '戳':
+            console.log(sourceId);
+            return `戳屁戳（版本：v${require('./package.json').version}）`;
+          case '有啥':
+            return map(await Shop.find({ sourceId }), parseShop).join('\n') || '不知道（選項尚未建立）';
+          case '可吃':
+            if (!name || await Shop.findOne({ name, sourceId })) {
+              return `不要（${name} 建立失敗）`;
+            } else if (name.length > MAX_SHOP_NAME_LEN) {
+              return `不要（${name.slice(0, 15)}... 建立失敗）`;
+            } else {
+              let closed = [];
+              let rate = 1;
+              chain(params).slice(2).each(param => {
+                switch (param[0]) {
+                  case '-':
+                    closed = chain([...param].sort()).map(day => day % 7).sortedUniq().filter(day => !isNaN(day)).value();
+                    break;
+                  case '.':
+                    rate = toNumber(param) || rate;
+                    break;
+                }
+              }).value();
+              const shop = new Shop({ name, sourceId, closed, rate });
+              return `好（${parseShop(await shop.save())} 已建立）`;
+            }
+          case '吃啥':
+            // t? help deciding
+            const filterList = chain(params).filter(param => param[0] === '-').map(param => param.slice(1)).value();
+            const shops = await Shop.find({ name: { $nin: filterList }, sourceId, closed: { $ne: new Date().getDay() } }) // timezone
+            let shop;
+            do {
+              shop = sample(shops);
+            } while (shop && shop.rate < Math.random());
+            return shop ? parseShop(shop) : '不知道（沒有選項）';
+          case '不吃':
+            return (await Shop.deleteOne({ name, sourceId })).deletedCount ? `不吃就不吃（${name} 已刪除）` : `你要確定欸（${name} 不存在）`;
+          case '不吃了':
+            await Shop.deleteMany({ sourceId });
+            return '不吃就不吃（已清除）';
+          case '很匯':
+            return map(await Shop.find({ sourceId }), shop => {
+              const { name, closed, rate } = pick(shop, ['name','closed','rate']);
+              return `可吃 ${name}${closed.length ? ' -' + closed.join('') : ''}${rate - 1 ? ' ' + rate.toString().slice(1) : ''}`;
+            }).join('\n');
+          case '怎麼吃':
+            break;
+          default:
+            if (source.type === 'user')
+              return '公鯊小';
+            break;
+        }
+      }))).join('\n'));
     }
-  }));
+  });
   ctx.status = 200;
 });
 
